58f5b927ed03937b486e29ef7b02d24f33e62bff
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-11-18 23:35:32 +0100

    [FRONT] Home {HeroSlider.jsx,hero-slider.css.}

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -23,11 +23,11 @@ const HeroSlider = () => {
     <div className="slider__item slider__item-01 mt0">
         <Container>
             <div className="slider__content">
-                <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-                <h1 className="text-light mb-4">Reserv Now and Get 50% Off</h1>
+                <h4 className="text-light mb-3">À louer 70 $ par jour</h4>
+                <h1 className="text-light mb-4">Réservez maintenant et obtenez 50% de réduction</h1>
 
                 <button className="btn reserve__btn mt-4">
-                    <Link to="/cars">Reserve Now</Link>
+                    <Link to="/cars">Réservez maintenant</Link>
                 </button>
             </div>
         </Container>
@@ -36,11 +36,11 @@ const HeroSlider = () => {
     <div className="slider__item slider__item-02 mt0">
         <Container>
             <div className="slider__content">
-                <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-                <h1 className="text-light mb-4">Reserv Now and Get 50% Off</h1>
+                <h4 className="text-light mb-3">À louer 70 $ par jour</h4>
+                <h1 className="text-light mb-4">Réservez maintenant et obtenez 50% de réduction</h1>
 
                 <button className="btn reserve__btn mt-4">
-                    <Link to="/cars">Reserve Now</Link>
+                    <Link to="/cars">Réservez maintenant</Link>
                 </button>
             </div>
         </Container>
@@ -49,11 +49,11 @@ const HeroSlider = () => {
     <div className="slider__item slider__item-03 mt0">
         <Container>
             <div className="slider__content">
-                <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-                <h1 className="text-light mb-4">Reserv Now and Get 50% Off</h1>
+                <h4 className="text-light mb-3">À louer 70 $ par jour</h4>
+                <h1 className="text-light mb-4">Réservez maintenant et obtenez 50% de réduction</h1>
 
                 <button className="btn reserve__btn mt-4">
-                    <Link to="/cars">Reserve Now</Link>
+                    <Link to="/cars">Réservez maintenant</Link>
                 </button>
             </div>
         </Container>
