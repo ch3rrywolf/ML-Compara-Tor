8e3b962f867325193ae73198bce9d385e5e63406
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-11-19 16:14:16 +0100

    [FRONT] Home {CarItem.jsx, car-item.css}

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ import { Container, Row, Col } from "reactstrap";
 import FindCarForm from "../components/UI/FindCarForm";
 import AboutSection from "../components/UI/AboutSection";
 import ServicesList from "../components/UI/ServicesList";
+import carData from '../assets/data/carData';
+import CarItem from "../components/UI/CarItem";
 
 const Home = () => {
   return (
@@ -41,6 +43,20 @@ const Home = () => {
             </Col>
 
             <ServicesList />
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+            <h6 className="section__subtitle">Come with</h6>
+            <h2 className="section__title">Hot Offers</h2>
+            </Col>
+            {carData.slice(0, 6).map((item) => (
+              <CarItem item={item} key={item.id} />
+            ))}
           </Row>
         </Container>
       </section>
