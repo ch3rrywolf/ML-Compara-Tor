62994bafa7cd96b2b3a78ce93f8a445ff60d9a2e
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-11-19 15:28:00 +0100

    [FRONT] Home {ServicesList.jsx, services-list.css}

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
 import FindCarForm from "../components/UI/FindCarForm";
 import AboutSection from "../components/UI/AboutSection";
+import ServicesList from "../components/UI/ServicesList";
 
 const Home = () => {
   return (
@@ -30,6 +31,19 @@ const Home = () => {
       </section>
 
       <AboutSection />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+            <h6 className="section__subtitle">See our</h6>
+            <h2 className="section__title">Popular Services</h2>
+            </Col>
+
+            <ServicesList />
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   );
 };
